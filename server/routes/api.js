82f11c67494820b36cbca37c28b40f2b2d1dedcf
AlMahmud22 <93167613+AlMahmud22@users.noreply.github.com>
2025-01-02 08:30:02 +0800

    Commit message

--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createUser } = require('../controllers/userController');
-
-// @route post api/users
-// @desc Regsiter a user
-// @access Public
-router.post('/users', createUser);
-module.exports = router;
